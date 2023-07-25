cc4b7e707b33a91a124a5459f29e9bd0f6e53ce8
Sergei Driga <drigaserge@gmail.com>
2023-07-25 10:58:37 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
 
+    static char[] array = new char[36];
+
+    private static void fillArray() {
+        for (int i = 0; i <= 9; i++) {
+            array[i] = (char) ('0' + i);
+        }
+        for (int i = 10; i < array.length; i++) {
+            array[i] = (char) ('A' + i - 10);
+        }
+    }
     public static void main(String[] args) {
+        fillArray();
         task1();
         task2();
         task3();
@@ -35,42 +48,96 @@ public class Main {
 
     public static void task2 () {
         System.out.println("Working with digit systems, part I");
-        int check = convertTo10DigitSystem("211", 3);
-        System.out.println(check);
+        System.out.println("Converting to 10 digit system");
+        System.out.println(convertTo10DigitSystem("211", 3));
+
+        System.out.println("Converting to any digit from 10 digit");
+        System.out.println(convertFrom10DigitToAnySystem(234,3));
     }
 
     private static int convertTo10DigitSystem(String number, int n) {
         int sum = 0;
 
-        char[] array = new char[36];
-
-        for (int i = 0; i <= 9; i++) {
-            array[i] = (char) ('0' + i);
-        }
-        for (int i = 10; i < array.length; i++) {
-            array[i] = (char) ('A' + i - 10);
-        }
-
-        for (int i = 0; i < number.length(); i++) {
-            double pow = Math.pow(n, number.length() - i - 1);
+        int multiplier = 1;
+        for (int i = number.length() - 1; i >= 0; i--) {
+//            double pow = Math.pow(n, number.length() - i - 1);
             if (number.charAt(i) <= '9') {
-                sum = sum + (number.charAt(i) - '0') * (int) pow;
+                sum = sum + (number.charAt(i) - '0') * multiplier;
             } else {
-                sum = sum + (number.charAt(i) - 'A' + 10) * (int) pow;
+                sum = sum + (number.charAt(i) - 'A' + 10) * multiplier;
             }
+            multiplier *= n;
         }
         return sum;
     }
-    public static void task3 () {
+
+    private static String convertFrom10DigitToAnySystem (int number, int n) {
+        StringBuilder result = new StringBuilder();
+        while (number != 0) {
+            char temp = array[number%n];
+            result.insert(0,temp);
+            number/=n;
+        }
+        return result.toString();
+    }
+
+    public static void task3 () { //Recursive function training
         System.out.println("Task 3");
+        System.out.println("Input n:");
+//        Scanner scanner = new Scanner(System.in);
+//        int n = scanner.nextInt();
+        int n = 10;
+        System.out.println(recursiveFunction(n));
+    }
+
+    public static int recursiveFunction(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        if (n == 3) {
+            return 3;
+        }
+            return recursiveFunction(n - 1) + recursiveFunction(n - 2) + recursiveFunction(n - 3);
     }
 
     public static void task4 () {
         System.out.println("Task 4");
+        int n = 9;
+        System.out.println("Sum equals to " + mySum(n));
     }
+
+    public static int mySum(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        if (n == 3) {
+            return 4;
+        }
+        return n + mySum(n - 2);
+    }
+
 
     public static void task5 () {
         System.out.println("Task 5");
+        int n = 10;
+        System.out.println("The sum equals to " + findSum(n));
+    }
+
+    public static int findSum(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 3;
+        }
+
+        return n + findSum(n - 1);
     }
 
     public static void task6 () {
@@ -80,7 +147,4 @@ public class Main {
     public static void task7 () {
         System.out.println("Task 7");
     }
-
-
-
 }
